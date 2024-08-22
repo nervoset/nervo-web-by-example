9dbed69474641b3fb2b14befcb3411e0af6a557a
cypherkitty <bynull@meta-secret.org>
2024-08-22 01:11:50 -0700

    w3a chat: add header text

--- w3a-react-js/w3a-react-app/src/App.tsx
+++ w3a-react-js/w3a-react-app/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <div style={{ position: 'fixed', bottom: '20px'}}>
             <div style={{height: '400px', overflow: 'auto'}}>
-                <nervo-chat></nervo-chat>
+                <nervo-chat header="Ai Agent" title="Web Academy" subtitle="Crypto"></nervo-chat>
             </div>
         </div>
     )
