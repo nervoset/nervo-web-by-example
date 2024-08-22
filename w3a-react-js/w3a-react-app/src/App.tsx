5a0dcd0c6f173774a4a1bfdc9ce60f1c0b2d075a
cypherkitty <bynull@meta-secret.org>
2024-08-21 23:09:25 -0700

    w3a initial example

---
+++ w3a-react-js/w3a-react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div style={{ position: 'fixed', bottom: '20px'}}>
+            <div style={{height: '400px', overflow: 'auto'}}>
+                <nervo-chat></nervo-chat>
+            </div>
+        </div>
+    )
+}
+
+export default App
