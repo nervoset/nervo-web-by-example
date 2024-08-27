9ee9a90db06b38ef38d9cb9e4b4e43cd0bb99816
cypherkitty <bynull@meta-secret.org>
2024-08-27 02:08:17 -0700

    migrate to github pages

--- w3a-react-js/w3a-react-app/src/App.tsx
+++ w3a-react-js/w3a-react-app/src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
 
     return (
-        <div style={{ position: 'fixed', bottom: '20px'}}>
-            <div style={{height: '400px', overflow: 'auto'}}>
+        <div>
+            <div>
                 <nervo-chat header="Ai Agent" title="Web Academy" subtitle="Crypto"></nervo-chat>
             </div>
         </div>
